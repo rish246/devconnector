04976c95675c23694211d75367f2cba743d767a5
Rishabh Katna <rishabhkatna@Rishabhs-MacBook-Air.local>
2024-10-22 20:13:36 +0530

    refactored posts component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,36 +1,29 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import PostCreate from './PostCreate';
 import PostList from './PostList';
 import { loadUser } from '../../slices/auth';
 import Spinner from '../layouts/Spinner';
 
-class Posts extends Component {
-	componentDidMount() {
-		this.props.loadUser();
-	}
-	render() {
-		if (this.props.loading) {
-			return (
-				<Fragment>
-					<Spinner />
-				</Fragment>
-			);
-		}
-		return (
-			<div>
-				<PostCreate />
-				<PostList />
-			</div>
-		);
-	}
-}
+const Posts = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading); // Get loading state from Redux
 
-const mapStateToProps = (state) => {
-	const { loading } = state.post;
-	return { loading };
+  useEffect(() => {
+    dispatch(loadUser()); // Dispatch loadUser action on component mount
+  }, [dispatch]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <PostCreate />
+      <PostList />
+    </div>
+  );
 };
-export default connect(null, { loadUser })(Posts);
 
-// authenticate the user
+export default Posts;
