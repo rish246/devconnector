91638a7dffb5724404d962fa600e4396d69e22bc
Rishabh Katna <rishabhkatna@Rishabhs-MacBook-Air.local>
2024-10-22 17:28:03 +0530

    fixed: edit profile form

--- client/src/components/Profiles/ProfileForm.js
+++ client/src/components/Profiles/ProfileForm.js
@@ -35,7 +35,7 @@ class ProfileForm extends Component {
 	onSubmit = (formValues) => {
 		console.log({ formValues });
 		// we have formValues ... now we can make an action creator to use the form values to make post req to the backend
-		this.props.createProfile({ formValues }, this.props.edit);
+		this.props.createProfile({ formValues, edit: this.props.edit });
 	};
 
 	render() {
