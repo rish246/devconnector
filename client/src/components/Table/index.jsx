9753d8c03edb10b0a2c56fb61a6569e51a11cff9
Rishabh Katna <rishabhkatna@Rishabhs-MacBook-Air.local>
2025-02-11 23:27:01 +0530

    UI refactor for table

--- client/src/components/Table/index.jsx
+++ client/src/components/Table/index.jsx
@@ -3,25 +3,37 @@ import "./Table.css";
 import classNames from "classnames";
 import { nanoid } from "nanoid";
 
-function Table({ config, data, className }) {
-    const header = config.map(({ name }) => {
+function Table({ config, data, className, headerClassName }) {
+    const header = config.map(({ name, header }) => {
+        if (header) {
+            return header();
+        }
         return (
-            <th className="table-header" key={nanoid()}>
+            <th className="table-header border" key={nanoid()}>
                 {name}
             </th>
         );
     });
 
     const body = data.map((row) => {
-        return <tr>{row.map((cell, idx) => config[idx].render(cell))}</tr>;
+        return (
+            <tr className="text-center border">
+                {row.map((cell, idx) => config[idx].render(cell))}
+            </tr>
+        );
     });
 
-    const finalClassName = classNames("table", "table-bordered", className);
+    const finalClassName = classNames(
+        // "border",
+        // "w-[100%]",
+        // "h-[100%]",
+        className
+    );
 
     return (
         <table className={finalClassName}>
             <thead>
-                <tr>{header}</tr>
+                <tr className={headerClassName}>{header}</tr>
             </thead>
             <tbody>{body}</tbody>
         </table>
--- client/src/pages/dashboard/Experience.jsx
+++ client/src/pages/dashboard/Experience.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import Table from "../../components/Table";
+import classNames from "classnames";
+import { FaTrash } from "react-icons/fa";
 
-export function Experience({ experience, onDelete }) {
+const TableHeader = ({ title }) => {
+    return (
+        <th className="bg-blue-50 border py-3 text-sm font-semibold text-gray-700 rounded-2xl text-center">
+            {title}
+        </th>
+    );
+};
+
+export function Experience({ experience, onDelete, className }) {
     const config = [
         {
             name: "Company",
-            render: (company) => <td>{company}</td>,
+            render: (company) => (
+                <td className="px-4 py-2 border">{company}</td>
+            ),
+            header: () => <TableHeader title="Company" />,
         },
         {
             name: "Title",
-            render: (title) => <td>{title}</td>,
+            render: (title) => <td className="px-4 py-2 border">{title}</td>,
+            header: () => <TableHeader title="Title" />,
         },
         {
             name: "Years",
             render: ({ from, to }) => (
-                <td>
-                    {from} - {to || "current"}
+                <td className="px-4 py-2 border">
+                    {from} - {to || "Current"}
                 </td>
             ),
+            header: () => <TableHeader title="Years" />,
+        },
+        {
+            name: "Actions",
+            render: (id) => (
+                <td className="px-4 py-3 text-center">
+                    <button
+                        className="p-2 text-red-600 transition-colors duration-200 rounded-md hover:bg-red-100 hover:text-red-700 hover:cursor-pointer"
+                        onClick={() => onDelete(id)}
+                    >
+                        <FaTrash />
+                    </button>
+                </td>
+            ),
+            header: () => <TableHeader title="Actions" />,
         },
     ];
 
@@ -25,10 +54,25 @@ export function Experience({ experience, onDelete }) {
         return <p className="text-muted">No experience records found.</p>;
     }
 
-    const data = experience.map(({ title, company, from, to }) => {
-        return [title, company, 3];
+    const data = experience.map(({ company, title, from, to, id }) => {
+        return [company, title, { from, to }, id];
     });
-    return <Table config={config} data={data} />;
+
+    const finalClassName = classNames(
+        "rounded-lg shadow-md w-full overflow-hidden",
+        className
+    );
+
+    return (
+        <div className="my-6">
+            <Table
+                config={config}
+                data={data}
+                className={finalClassName}
+                headerClassName="bg-blue-50 border-b"
+            />
+        </div>
+    );
 }
 
 export default Experience;
